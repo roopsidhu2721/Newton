49f241b21f0775e0e8c7e2aa6a0852f7121b9b59
roopsidhu2721 <69040930+roopsidhu2721@users.noreply.github.com>
2021-01-21 15:01:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var rope1,rope2,rope3, rope4,rope5;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roofObject=new Roof(width/2,height/4,width/7,20);
+
+	bobDiameter=40;
+
+	startBobPositionX=width/2;
+	startBobPositionY=height/4+500;
+	bobObject1=new Bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+	
+	
+	
+
+	rope1=new Rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
+
+	
+
+	Engine.run(engine);
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+  roofObject.display();
+
+  rope1.display()
+ 	
+  bobObject1.display();
+  
+  
+  
+	
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+
+  	}
+}
+
+
